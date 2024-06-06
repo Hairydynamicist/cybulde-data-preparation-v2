0636b91fafb4a97fff4a46caee75fad3e9aa8d18
hairydynamicist <john.b.murray@gmail.com>
2024-06-06 13:49:25 +0100

    moved forward onto a new error

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -2,17 +2,11 @@ from cybulde.config_schemas.data_processing_config_schema import DataProcessingC
 from cybulde.utils.config_utils import get_config
 from cybulde.utils.gcp_utils import access_secret_version
 from cybulde.utils.data_utils import get_raw_data_with_version
+from hydra.utils import instantiate
 
 
 @get_config(config_path="../configs", config_name="data_processing_config")
-def process_data(config: DataProcessingConfig) -> None:
-    print(config)
-    from omegaconf import OmegaConf
-    print(60*"#")
-    print(OmegaConf.to_yaml(config))
-
-    return
-   
+def process_data(config: DataProcessingConfig) -> None:  
     github_access_token = access_secret_version(config.infrastructure.project_id, config.github_access_token_scret_id)
 
     get_raw_data_with_version(
@@ -23,6 +17,12 @@ def process_data(config: DataProcessingConfig) -> None:
         github_user_name=config.github_user_name,
         github_access_token=github_access_token
     )
+ 
+    dataset_reader_manager = instantiate(config.dataset_reader_manager)
+
+    df = dataset_reader_manager.read_data()
+
+    print(df.head())
 
 
 if __name__ == "__main__":
