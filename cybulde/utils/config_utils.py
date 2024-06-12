bd0cf777355730b76cefbbfd670d34889aa5ab49
hairydynamicist <john.b.murray@gmail.com>
2024-06-12 11:59:27 +0100

    started building generate_final_config.py

--- cybulde/utils/config_utils.py
+++ cybulde/utils/config_utils.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+import argparse
 
 from typing import Any, Optional
 
@@ -7,7 +8,11 @@ import hydra
 import yaml
 
 from hydra.types import TaskFunction
+from hydra import compose, initialize
+
 from omegaconf import DictConfig, OmegaConf
+
+from hydra import compose, initialize
 
 from cybulde.config_schemas import data_processing_config_schema
 
@@ -35,3 +40,26 @@ def setup_logger() -> None:
     with open("./cybulde/configs/hydra/job_logging/custom.yaml", "r") as stream:
         config = yaml.load(stream, Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
+
+
+def config_args_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--config-path", type=str, default="../configs/", help="Directory of the config files")
+    parser.add_argument("--config-name", type=str, required=True, help="Name of the config file")
+    parser.add_argument("--overrides", nargs="*", help="Hydra config overrides", default=[])
+
+    return parser.parse_args()
+
+
+def compose_config(config_path: str, config_name: str, overrides: Optional[list[str]] = None) -> Any:
+    setup_config()
+    setup_logger()
+
+    if overrides is None:
+        overrides = []
+
+    with initialize(version_base=None, config_path=config_path, job_name="config-compose"):
+        dict_config = compose(config_name=config_name, overrides=overrides)
+        config = OmegaConf.to_object(dict_config)
+    return config
